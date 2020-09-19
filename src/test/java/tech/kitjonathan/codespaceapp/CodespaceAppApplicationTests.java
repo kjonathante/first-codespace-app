b91bb0409815ee7b5d46486a4e5a533eccc44e78
Kit Jonathan Te <kjonathante@gmail.com>
2020-09-18 23:20:41 -0700

    Initial

---
+++ src/test/java/tech/kitjonathan/codespaceapp/CodespaceAppApplicationTests.java
@@ -0,0 +1,13 @@
+package tech.kitjonathan.codespaceapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodespaceAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
